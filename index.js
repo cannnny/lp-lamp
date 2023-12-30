9529b02393e34fe7942847fe5564b9902f8764e8
cany <couleurvide1008@gmail.com>
2023-12-30 11:28:07 +0900

    モーダル作成、アニメーション・フォント設定

--- index.js
+++ index.js
@@ -1,8 +1,12 @@
 // モーダル開閉
-const menuButton = document.getElementById("js-header-menu");
+const menuButton = document.getElementById("js-menu");
+// const menuLine = document.getElementsByClassName("js-menu-line");
+// const menuText = document.getElementById("js-menu-text");
 const modal = document.getElementById("js-modal");
 
 menuButton.addEventListener("click", function () {
-  menuButton.classList.toggle("close");
-  modal.classList.toggle("active");
+  menuButton.classList.toggle("is-open");
+  // menuLine.classList.toggle("is-open");
+  // menuText.classList.toggle("is-open");
+  modal.classList.toggle("is-open");
 });
