d573fe14206b5b52052f47daf4476206c039b392
cany <couleurvide1008@gmail.com>
2024-01-04 14:24:37 +0900

    buttonとimgに付与しているクラスを修正

--- index.js
+++ index.js
@@ -1,12 +1,8 @@
 // モーダル開閉
 const menuButton = document.getElementById("js-menu");
-// const menuLine = document.getElementsByClassName("js-menu-line");
-// const menuText = document.getElementById("js-menu-text");
 const modal = document.getElementById("js-modal");
 
 menuButton.addEventListener("click", function () {
   menuButton.classList.toggle("is-open");
-  // menuLine.classList.toggle("is-open");
-  // menuText.classList.toggle("is-open");
   modal.classList.toggle("is-open");
 });
